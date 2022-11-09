6722c45ad641fbb498b1c878e3a5b2cd9a059cda
rivsh030@likelion.org <rivsh030@likelion.org>
2022-11-09 18:50:50 +0900

    admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,10 +4,19 @@ import React from 'react'
 
 export default function Admin() {
   return (
+    <>
     <div css={style}>this is Admin page</div>
+    <div css={adminStyle}>this is Admin page</div>
+    </>
+
   )
 }
 
 const style = css`
     color: #000;
+    margin: auto;
+`
+const adminStyle = css`
+    color: red;
+    margin: auto;
 `
