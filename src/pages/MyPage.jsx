8c0dc33a69275ab7c5da1a661524d9c756ad02f3
longdoheun <george1119@naver.com>
2022-11-09 17:19:32 +0900

    components folder added

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function MyPage() {
   return (
     <>
-        <div css={style}>this is MyPage</div>
+      <div css={style}>this is MyPage</div>
     </>
   )
 }
