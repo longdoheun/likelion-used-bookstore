b849e991ad8bf4355fe05f9e99c9232d48bb8b55
longdoheun <george1119@naver.com>
2022-11-09 17:11:33 +0900

    first settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import MyPage from "./pages/MyPage";
+import NotFound from "./pages/NotFound";
+import Payment from "./pages/Payment";
+import Products from "./pages/Products";
+import ProductsDetail from "./pages/ProductsDetail";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" css={style}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main/>}></Route>
+        <Route path="/admin" element={<Admin/>}></Route>
+        <Route path="/mypage" element={<MyPage/>}></Route>
+        <Route path="/products" element={<Products/>}></Route>
+        <Route path="/products/:product_id" element={<ProductsDetail/>}></Route>
+        <Route path="/products/:product_id/payment" element={<Payment/>}></Route>
+        <Route path="/signin" element={<SignIn/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
+const style = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
 
 export default App;
---
+++ src/pages/Payment.jsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+import React from 'react'
+
+export default function Payment() {
+  return (
+    <div css={style}>this is Payment page</div>
+  )
+}
+
+const style = css`
+    color : #000;
+`
